cb1d22a3748bad51a818ff263542f2e455132c3e
Zain <Zain890@gmail.com>
2022-07-26 15:43:51 -0700

    Refactorings and small cleanups.
    random.js removed
    todos route changed to mongo

---
+++ src/routes/mongo/index.svelte
@@ -0,0 +1,8 @@
+<script>
+
+</script>
+
+
+<h1 class="text-3xl font-bold underline">MONGO ROUTE</h1>
+<div type="text">bookList</div>
+<pre>Insert Booklist Return Results Here</pre>
--- src/routes/mongo/index.ts
+++ src/routes/mongo/index.ts
@@ -1,26 +1,30 @@
 import clientPromise from '$lib/mongodb-client';
 import type { MongoClient } from 'mongodb';
 
+let serverResponse;
+
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function GET() {
 	const client = await clientPromise;
-  const db = client.db("Books");
+	const db = client.db('Books');
 	const collection = db.collection('Books');
-	const books = collection.find({ pageCount: { $gt: 1100 } }).toArray((err, res) => {
-		if (err) console.log('err', err);
-		console.log('res', res);
-	});
-
-  return {
+	
+	//Authors note: If you want the page to be able to render the results of your DB, you might wanna
+	//use an await, to give your server a chance to respond before you immediately render {}.
+	let serverResponse = await collection.find({ pageCount: { $gt: 1100 } }).toArray();
+	
+	return {
 		status: 200,
 		headers: {
-			'access-control-allow-origin': '*'
+			'access-control-allow-origin': '*',
+			'Content-Type': 'application/json'
 		},
-		body: {
-      books
-    },
+		body: {serverResponse}
 	};
 }
+console.log("SR: " + serverResponse)
+export default serverResponse;
+
 
 export async function POST() {
 	const dbConnection = await clientPromise;
